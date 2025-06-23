a8aa3ce2fb59d27a0ede29208bb9e01f492b8dab
xanstar6067 <district6067@gmail.com>
2025-06-23 10:57:15 +0300

    Создана новая форма. Настройка цвета. (Пока не интегрирована в общий функционал)

--- FractalExplorer/FractalExplorer/SelectorsForms/ColorConfigurationForm.Designer.cs
+++ FractalExplorer/FractalExplorer/SelectorsForms/ColorConfigurationForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FractalExplorer.SelectorsForms
+﻿namespace FractalExplorer.Core // Убедитесь, что пространство имен совпадает с вашим
 {
     partial class ColorConfigurationForm
     {
@@ -29,11 +29,279 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lbPalettes = new System.Windows.Forms.ListBox();
+            this.panelPreview = new System.Windows.Forms.Panel();
+            this.lbColorStops = new System.Windows.Forms.ListBox();
+            this.btnAddColor = new System.Windows.Forms.Button();
+            this.btnRemoveColor = new System.Windows.Forms.Button();
+            this.btnEditColor = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.checkIsGradient = new System.Windows.Forms.CheckBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbPalettes
+            // 
+            this.lbPalettes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbPalettes.FormattingEnabled = true;
+            this.lbPalettes.ItemHeight = 16;
+            this.lbPalettes.Location = new System.Drawing.Point(8, 23);
+            this.lbPalettes.Name = "lbPalettes";
+            this.lbPalettes.Size = new System.Drawing.Size(220, 324);
+            this.lbPalettes.TabIndex = 0;
+            this.lbPalettes.SelectedIndexChanged += new System.EventHandler(this.lbPalettes_SelectedIndexChanged);
+            // 
+            // panelPreview
+            // 
+            this.panelPreview.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelPreview.Location = new System.Drawing.Point(9, 79);
+            this.panelPreview.Name = "panelPreview";
+            this.panelPreview.Size = new System.Drawing.Size(469, 40);
+            this.panelPreview.TabIndex = 1;
+            this.panelPreview.Paint += new System.Windows.Forms.PaintEventHandler(this.panelPreview_Paint);
+            // 
+            // lbColorStops
+            // 
+            this.lbColorStops.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbColorStops.FormattingEnabled = true;
+            this.lbColorStops.ItemHeight = 16;
+            this.lbColorStops.Location = new System.Drawing.Point(9, 149);
+            this.lbColorStops.Name = "lbColorStops";
+            this.lbColorStops.Size = new System.Drawing.Size(341, 196);
+            this.lbColorStops.TabIndex = 2;
+            // 
+            // btnAddColor
+            // 
+            this.btnAddColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddColor.Location = new System.Drawing.Point(356, 149);
+            this.btnAddColor.Name = "btnAddColor";
+            this.btnAddColor.Size = new System.Drawing.Size(122, 30);
+            this.btnAddColor.TabIndex = 3;
+            this.btnAddColor.Text = "Добавить...";
+            this.btnAddColor.UseVisualStyleBackColor = true;
+            this.btnAddColor.Click += new System.EventHandler(this.btnAddColor_Click);
+            // 
+            // btnRemoveColor
+            // 
+            this.btnRemoveColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemoveColor.Location = new System.Drawing.Point(356, 221);
+            this.btnRemoveColor.Name = "btnRemoveColor";
+            this.btnRemoveColor.Size = new System.Drawing.Size(122, 30);
+            this.btnRemoveColor.TabIndex = 5;
+            this.btnRemoveColor.Text = "Удалить цвет";
+            this.btnRemoveColor.UseVisualStyleBackColor = true;
+            this.btnRemoveColor.Click += new System.EventHandler(this.btnRemoveColor_Click);
+            // 
+            // btnEditColor
+            // 
+            this.btnEditColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEditColor.Location = new System.Drawing.Point(356, 185);
+            this.btnEditColor.Name = "btnEditColor";
+            this.btnEditColor.Size = new System.Drawing.Size(122, 30);
+            this.btnEditColor.TabIndex = 4;
+            this.btnEditColor.Text = "Изменить...";
+            this.btnEditColor.UseVisualStyleBackColor = true;
+            this.btnEditColor.Click += new System.EventHandler(this.btnEditColor_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Location = new System.Drawing.Point(9, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(341, 22);
+            this.txtName.TabIndex = 6;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // checkIsGradient
+            // 
+            this.checkIsGradient.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkIsGradient.AutoSize = true;
+            this.checkIsGradient.Location = new System.Drawing.Point(356, 43);
+            this.checkIsGradient.Name = "checkIsGradient";
+            this.checkIsGradient.Size = new System.Drawing.Size(91, 20);
+            this.checkIsGradient.TabIndex = 7;
+            this.checkIsGradient.Text = "Градиент";
+            this.checkIsGradient.UseVisualStyleBackColor = true;
+            this.checkIsGradient.CheckedChanged += new System.EventHandler(this.checkIsGradient_CheckedChanged);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnNew.Location = new System.Drawing.Point(8, 353);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(109, 30);
+            this.btnNew.TabIndex = 8;
+            this.btnNew.Text = "Новая";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(123, 353);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(105, 30);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSave.Location = new System.Drawing.Point(12, 420);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(170, 30);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Сохранить палитры";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(500, 420);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(120, 30);
+            this.btnOK.TabIndex = 11;
+            this.btnOK.Text = "Применить";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(626, 420);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(120, 30);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AnyColor = true;
+            this.colorDialog1.FullOpen = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.lbPalettes);
+            this.groupBox1.Controls.Add(this.btnNew);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 393);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Список палитр";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.panelPreview);
+            this.groupBox2.Controls.Add(this.lbColorStops);
+            this.groupBox2.Controls.Add(this.btnAddColor);
+            this.groupBox2.Controls.Add(this.checkIsGradient);
+            this.groupBox2.Controls.Add(this.btnEditColor);
+            this.groupBox2.Controls.Add(this.btnRemoveColor);
+            this.groupBox2.Location = new System.Drawing.Point(267, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(484, 393);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Редактор палитры";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Название палитры:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ключевые цвета:";
+            // 
+            // ColorConfigurationForm
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ColorConfigurationForm";
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(763, 462);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(781, 509);
+            this.Name = "ColorConfigurationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Настройка цветовых палитр";
+            this.Load += new System.EventHandler(this.ColorConfigurationForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbPalettes;
+        private System.Windows.Forms.Panel panelPreview;
+        private System.Windows.Forms.ListBox lbColorStops;
+        private System.Windows.Forms.Button btnAddColor;
+        private System.Windows.Forms.Button btnRemoveColor;
+        private System.Windows.Forms.Button btnEditColor;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.CheckBox checkIsGradient;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FractalExplorer/FractalExplorer/SelectorsForms/ColorConfigurationForm.cs
+++ FractalExplorer/FractalExplorer/SelectorsForms/ColorConfigurationForm.cs
@@ -1,20 +1,212 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
-namespace FractalExplorer.SelectorsForms
+namespace FractalExplorer.Core
 {
     public partial class ColorConfigurationForm : Form
     {
-        public ColorConfigurationForm()
+        private readonly PaletteManager _paletteManager;
+        private ColorPalette _selectedPalette;
+
+        public ColorConfigurationForm(PaletteManager paletteManager)
         {
             InitializeComponent();
+            _paletteManager = paletteManager;
+        }
+
+        private void ColorConfigurationForm_Load(object sender, EventArgs e)
+        {
+            PopulatePaletteList();
+            // Выбираем активную палитру при загрузке
+            lbPalettes.SelectedItem = _paletteManager.ActivePalette.Name;
+        }
+
+        private void PopulatePaletteList()
+        {
+            lbPalettes.Items.Clear();
+            foreach (var palette in _paletteManager.Palettes)
+            {
+                string displayName = palette.IsBuiltIn ? $"{palette.Name} [Встроенная]" : palette.Name;
+                lbPalettes.Items.Add(displayName);
+            }
+        }
+
+        private void lbPalettes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbPalettes.SelectedIndex == -1) return;
+
+            // Находим палитру по реальному имени, отбрасывая "[Встроенная]"
+            string selectedName = lbPalettes.SelectedItem.ToString().Replace(" [Встроенная]", "");
+            _selectedPalette = _paletteManager.Palettes.FirstOrDefault(p => p.Name == selectedName);
+
+            if (_selectedPalette == null) return;
+
+            DisplayPaletteDetails();
+            UpdateControlsState();
+        }
+
+        private void DisplayPaletteDetails()
+        {
+            txtName.Text = _selectedPalette.Name;
+            checkIsGradient.Checked = _selectedPalette.IsGradient;
+
+            lbColorStops.Items.Clear();
+            foreach (var color in _selectedPalette.Colors)
+            {
+                lbColorStops.Items.Add(color.Name);
+            }
+            panelPreview.Invalidate();
+        }
+
+        private void UpdateControlsState()
+        {
+            bool isCustom = !_selectedPalette.IsBuiltIn;
+            txtName.Enabled = isCustom;
+            checkIsGradient.Enabled = isCustom;
+            lbColorStops.Enabled = isCustom;
+            btnAddColor.Enabled = isCustom;
+            btnEditColor.Enabled = isCustom;
+            btnRemoveColor.Enabled = isCustom;
+            btnDelete.Enabled = isCustom;
+        }
+
+        private void panelPreview_Paint(object sender, PaintEventArgs e)
+        {
+            if (_selectedPalette == null || _selectedPalette.Colors.Count == 0)
+            {
+                e.Graphics.Clear(Color.Black);
+                return;
+            }
+
+            if (_selectedPalette.Colors.Count == 1)
+            {
+                using (var brush = new SolidBrush(_selectedPalette.Colors[0]))
+                {
+                    e.Graphics.FillRectangle(brush, panelPreview.ClientRectangle);
+                }
+                return;
+            }
+
+            using (var lgb = new LinearGradientBrush(panelPreview.ClientRectangle, Color.Black, Color.Black, 0f))
+            {
+                var cb = new ColorBlend(_selectedPalette.Colors.Count);
+                cb.Colors = _selectedPalette.Colors.ToArray();
+                cb.Positions = Enumerable.Range(0, _selectedPalette.Colors.Count)
+                                         .Select(i => (float)i / (_selectedPalette.Colors.Count - 1))
+                                         .ToArray();
+                lgb.InterpolationColors = cb;
+                e.Graphics.FillRectangle(lgb, panelPreview.ClientRectangle);
+            }
+        }
+
+        // --- Обработчики кнопок для редактирования ---
+
+        private void btnAddColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _selectedPalette.Colors.Add(colorDialog1.Color);
+                DisplayPaletteDetails();
+            }
+        }
+
+        private void btnEditColor_Click(object sender, EventArgs e)
+        {
+            if (lbColorStops.SelectedIndex == -1) return;
+            colorDialog1.Color = _selectedPalette.Colors[lbColorStops.SelectedIndex];
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _selectedPalette.Colors[lbColorStops.SelectedIndex] = colorDialog1.Color;
+                DisplayPaletteDetails();
+            }
+        }
+
+        private void btnRemoveColor_Click(object sender, EventArgs e)
+        {
+            if (lbColorStops.SelectedIndex != -1 && _selectedPalette.Colors.Count > 1)
+            {
+                _selectedPalette.Colors.RemoveAt(lbColorStops.SelectedIndex);
+                DisplayPaletteDetails();
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            string newName = "Новая палитра";
+            int counter = 1;
+            while (_paletteManager.Palettes.Any(p => p.Name == newName))
+            {
+                newName = $"Новая палитра {++counter}";
+            }
+
+            var newPalette = new ColorPalette(newName, new List<Color> { Color.Black, Color.White }, true);
+            _paletteManager.Palettes.Add(newPalette);
+
+            PopulatePaletteList();
+            lbPalettes.SelectedItem = newPalette.Name;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (_selectedPalette != null && !_selectedPalette.IsBuiltIn)
+            {
+                if (MessageBox.Show($"Вы уверены, что хотите удалить палитру '{_selectedPalette.Name}'?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    _paletteManager.Palettes.Remove(_selectedPalette);
+                    PopulatePaletteList();
+                    lbPalettes.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            if (_selectedPalette != null && !_selectedPalette.IsBuiltIn)
+            {
+                _selectedPalette.Name = txtName.Text;
+            }
+        }
+
+        private void checkIsGradient_CheckedChanged(object sender, EventArgs e)
+        {
+            if (_selectedPalette != null && !_selectedPalette.IsBuiltIn)
+            {
+                _selectedPalette.IsGradient = checkIsGradient.Checked;
+            }
+        }
+
+        // --- Основные кнопки формы ---
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Обновляем имена в списке перед сохранением
+            int currentSelectedIndex = lbPalettes.SelectedIndex;
+            PopulatePaletteList();
+            lbPalettes.SelectedIndex = currentSelectedIndex;
+
+            _paletteManager.SaveCustomPalettes();
+            MessageBox.Show("Пользовательские палитры сохранены.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (_selectedPalette != null)
+            {
+                _paletteManager.ActivePalette = _selectedPalette;
+            }
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
         }
     }
 }
