e6f0d6088371d0edda6ae0ed56656102ed0bb3c8
xanstar6067 <district6067@gmail.com>
2025-06-25 10:01:54 +0300

    Реализован код новой формы.

--- FractalExplorer/FractalExplorer/Forms/SaveLoadDialogForm.Designer.cs
+++ FractalExplorer/FractalExplorer/Forms/SaveLoadDialogForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FractalExplorer.Forms
+﻿namespace FractalExplorer.Forms // Убедись, что namespace соответствует твоему
 {
     partial class SaveLoadDialogForm
     {
@@ -28,12 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxSaves = new System.Windows.Forms.ListBox();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            this.textBoxSaveName = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSaveAsNew = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.labelSavesList = new System.Windows.Forms.Label();
+            this.labelPreview = new System.Windows.Forms.Label();
+            this.labelSaveName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxSaves
+            // 
+            this.listBoxSaves.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxSaves.FormattingEnabled = true;
+            this.listBoxSaves.ItemHeight = 15;
+            this.listBoxSaves.Location = new System.Drawing.Point(12, 35);
+            this.listBoxSaves.Name = "listBoxSaves";
+            this.listBoxSaves.Size = new System.Drawing.Size(280, 289);
+            this.listBoxSaves.TabIndex = 0;
+            this.listBoxSaves.SelectedIndexChanged += new System.EventHandler(this.listBoxSaves_SelectedIndexChanged);
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxPreview.Location = new System.Drawing.Point(307, 35);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(365, 244);
+            this.pictureBoxPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxPreview.TabIndex = 1;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // textBoxSaveName
+            // 
+            this.textBoxSaveName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSaveName.Location = new System.Drawing.Point(307, 301);
+            this.textBoxSaveName.Name = "textBoxSaveName";
+            this.textBoxSaveName.Size = new System.Drawing.Size(256, 23);
+            this.textBoxSaveName.TabIndex = 2;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoad.Location = new System.Drawing.Point(497, 330);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(85, 28);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Загрузить";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSaveAsNew
+            // 
+            this.btnSaveAsNew.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSaveAsNew.Location = new System.Drawing.Point(569, 300);
+            this.btnSaveAsNew.Name = "btnSaveAsNew";
+            this.btnSaveAsNew.Size = new System.Drawing.Size(103, 24);
+            this.btnSaveAsNew.TabIndex = 3;
+            this.btnSaveAsNew.Text = "Сохранить как";
+            this.btnSaveAsNew.UseVisualStyleBackColor = true;
+            this.btnSaveAsNew.Click += new System.EventHandler(this.btnSaveAsNew_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDelete.Location = new System.Drawing.Point(12, 330);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(85, 28);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(588, 330);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(84, 28);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // labelSavesList
+            // 
+            this.labelSavesList.AutoSize = true;
+            this.labelSavesList.Location = new System.Drawing.Point(12, 13);
+            this.labelSavesList.Name = "labelSavesList";
+            this.labelSavesList.Size = new System.Drawing.Size(119, 15);
+            this.labelSavesList.TabIndex = 7;
+            this.labelSavesList.Text = "Список сохранений:";
+            // 
+            // labelPreview
+            // 
+            this.labelPreview.AutoSize = true;
+            this.labelPreview.Location = new System.Drawing.Point(307, 13);
+            this.labelPreview.Name = "labelPreview";
+            this.labelPreview.Size = new System.Drawing.Size(81, 15);
+            this.labelPreview.TabIndex = 8;
+            this.labelPreview.Text = "Предпросмотр:";
+            // 
+            // labelSaveName
+            // 
+            this.labelSaveName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelSaveName.AutoSize = true;
+            this.labelSaveName.Location = new System.Drawing.Point(307, 283);
+            this.labelSaveName.Name = "labelSaveName";
+            this.labelSaveName.Size = new System.Drawing.Size(103, 15);
+            this.labelSaveName.TabIndex = 9;
+            this.labelSaveName.Text = "Имя сохранения:";
+            // 
+            // SaveLoadDialogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SaveLoadDialogForm";
+            this.ClientSize = new System.Drawing.Size(684, 369);
+            this.Controls.Add(this.labelSaveName);
+            this.Controls.Add(this.labelPreview);
+            this.Controls.Add(this.labelSavesList);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSaveAsNew);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.textBoxSaveName);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.listBoxSaves);
+            this.MinimumSize = new System.Drawing.Size(550, 350);
+            this.Name = "SaveLoadDialogForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Менеджер сохранений";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SaveLoadDialogForm_FormClosing);
+            this.Load += new System.EventHandler(this.SaveLoadDialogForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxSaves;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
+        private System.Windows.Forms.TextBox textBoxSaveName;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSaveAsNew;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label labelSavesList;
+        private System.Windows.Forms.Label labelPreview;
+        private System.Windows.Forms.Label labelSaveName;
     }
 }
--- FractalExplorer/FractalExplorer/Forms/SaveLoadDialogForm.cs
+++ FractalExplorer/FractalExplorer/Forms/SaveLoadDialogForm.cs
@@ -1,20 +1,227 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; // Для Task
 using System.Windows.Forms;
+using FractalExplorer.Utilities; // Для ISaveLoadCapableFractal, FractalSaveStateBase
 
-namespace FractalExplorer.Forms
+namespace FractalExplorer.Forms // Убедись, что namespace соответствует твоему
 {
     public partial class SaveLoadDialogForm : Form
     {
-        public SaveLoadDialogForm()
+        private readonly ISaveLoadCapableFractal _ownerFractalForm;
+        private List<FractalSaveStateBase> _saves;
+        private Bitmap _currentPreviewBitmap; // Для корректного Dispose
+
+        public SaveLoadDialogForm(ISaveLoadCapableFractal ownerFractalForm)
         {
             InitializeComponent();
+            _ownerFractalForm = ownerFractalForm ?? throw new ArgumentNullException(nameof(ownerFractalForm));
+            // Устанавливаем заголовок окна в зависимости от типа фрактала
+            this.Text = $"Сохранение/Загрузка: {_ownerFractalForm.FractalTypeIdentifier}";
+        }
+
+        private void SaveLoadDialogForm_Load(object sender, EventArgs e)
+        {
+            LoadSavesList();
+            UpdateButtonsState();
+        }
+
+        private void LoadSavesList()
+        {
+            _saves = _ownerFractalForm.LoadAllSavesForThisType();
+            // Сортируем по дате, новые вверху (SaveFileManager теперь это делает при сохранении, но для надежности можно и здесь)
+            // _saves = _saves.OrderByDescending(s => s.Timestamp).ToList(); 
+
+            listBoxSaves.Items.Clear();
+            if (_saves != null) // Добавим проверку на null
+            {
+                foreach (var save in _saves)
+                {
+                    listBoxSaves.Items.Add($"{save.SaveName} ({save.Timestamp:yyyy-MM-dd HH:mm:ss})");
+                }
+            }
+
+            if (listBoxSaves.Items.Count > 0)
+            {
+                listBoxSaves.SelectedIndex = 0;
+            }
+            else
+            {
+                ClearPreview(); // Очищаем превью, если список пуст
+            }
+        }
+
+        private void listBoxSaves_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ClearPreview(); // Очищаем предыдущее превью
+            if (listBoxSaves.SelectedIndex >= 0 && _saves != null && listBoxSaves.SelectedIndex < _saves.Count)
+            {
+                var selectedState = _saves[listBoxSaves.SelectedIndex];
+                textBoxSaveName.Text = selectedState.SaveName; // Предзаполняем имя для удобства
+                RenderAndDisplayPreview(selectedState);
+            }
+            UpdateButtonsState();
+        }
+
+        private async void RenderAndDisplayPreview(FractalSaveStateBase state)
+        {
+            if (pictureBoxPreview.Width <= 0 || pictureBoxPreview.Height <= 0 || state == null)
+            {
+                ClearPreview();
+                return;
+            }
+
+            // Показываем заглушку "Загрузка..."
+            Bitmap loadingBmp = new Bitmap(pictureBoxPreview.Width, pictureBoxPreview.Height);
+            using (var g = Graphics.FromImage(loadingBmp))
+            {
+                g.Clear(Color.LightGray);
+                TextRenderer.DrawText(g, "Рендеринг превью...", Font, pictureBoxPreview.ClientRectangle, Color.Black, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+            }
+            pictureBoxPreview.Image = loadingBmp; // Старый _currentPreviewBitmap будет освобожден при присваивании нового или в ClearPreview
+
+            try
+            {
+                // Асинхронный рендер превью
+                Bitmap previewBitmap = await Task.Run(() => _ownerFractalForm.RenderPreview(state, pictureBoxPreview.Width, pictureBoxPreview.Height));
+
+                // Проверяем, что контрол все еще существует перед обновлением UI
+                if (pictureBoxPreview.IsHandleCreated && !pictureBoxPreview.IsDisposed)
+                {
+                    _currentPreviewBitmap?.Dispose(); // Освобождаем старый, если он был
+                    _currentPreviewBitmap = previewBitmap;
+                    pictureBoxPreview.Image = _currentPreviewBitmap;
+                }
+                else
+                {
+                    previewBitmap?.Dispose(); // Если контрола нет, просто освобождаем новый битмап
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка рендера превью: {ex.Message}");
+                Bitmap errorBmp = new Bitmap(pictureBoxPreview.Width, pictureBoxPreview.Height);
+                using (var g = Graphics.FromImage(errorBmp))
+                {
+                    g.Clear(Color.MistyRose);
+                    TextRenderer.DrawText(g, "Ошибка превью", Font, pictureBoxPreview.ClientRectangle, Color.Red, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+                }
+                _currentPreviewBitmap?.Dispose(); // Освобождаем предыдущий битмап (мог быть loadingBmp или старый _currentPreviewBitmap)
+                _currentPreviewBitmap = errorBmp;
+                if (pictureBoxPreview.IsHandleCreated && !pictureBoxPreview.IsDisposed)
+                {
+                    pictureBoxPreview.Image = _currentPreviewBitmap;
+                }
+            }
+        }
+
+        private void ClearPreview()
+        {
+            // Освобождаем предыдущий битмап перед тем, как очистить Image
+            _currentPreviewBitmap?.Dispose();
+            _currentPreviewBitmap = null;
+            pictureBoxPreview.Image = null;
+        }
+
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (listBoxSaves.SelectedIndex >= 0 && _saves != null && listBoxSaves.SelectedIndex < _saves.Count)
+            {
+                _ownerFractalForm.LoadState(_saves[listBoxSaves.SelectedIndex]);
+                this.DialogResult = DialogResult.OK; // Устанавливаем результат диалога
+                this.Close();
+            }
+        }
+
+        private void btnSaveAsNew_Click(object sender, EventArgs e)
+        {
+            string saveName = textBoxSaveName.Text.Trim();
+            if (string.IsNullOrWhiteSpace(saveName))
+            {
+                MessageBox.Show("Пожалуйста, введите имя для сохранения.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxSaveName.Focus();
+                return;
+            }
+
+            if (_saves == null) _saves = new List<FractalSaveStateBase>();
+
+            // Проверка на дубликат имени
+            var existingSave = _saves.FirstOrDefault(s => s.SaveName.Equals(saveName, StringComparison.OrdinalIgnoreCase));
+            if (existingSave != null)
+            {
+                if (MessageBox.Show($"Сохранение с именем '{saveName}' уже существует. Перезаписать?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    // Удаляем старое сохранение с таким же именем
+                    _saves.Remove(existingSave);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            var newState = _ownerFractalForm.GetCurrentStateForSave(saveName);
+            _saves.Add(newState);
+            _ownerFractalForm.SaveAllSavesForThisType(_saves); // Сохраняем весь обновленный список
+
+            // Перезагружаем и выбираем новый элемент
+            int previouslySelectedIndex = listBoxSaves.SelectedIndex; // Сохраняем, на всякий случай
+            LoadSavesList();
+
+            // Пытаемся найти и выбрать только что сохраненный элемент
+            int newIndex = -1;
+            for (int i = 0; i < _saves.Count; i++) // Ищем в _saves, так как listBoxSaves.Items - это строки
+            {
+                if (_saves[i].SaveName == saveName && _saves[i].Timestamp == newState.Timestamp)
+                {
+                    newIndex = i;
+                    break;
+                }
+            }
+            if (newIndex != -1 && newIndex < listBoxSaves.Items.Count)
+            {
+                listBoxSaves.SelectedIndex = newIndex;
+            }
+            else if (listBoxSaves.Items.Count > 0) // Если не нашли, выбираем первый или последний
+            {
+                listBoxSaves.SelectedIndex = 0; // или _saves.Count - 1
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxSaves.SelectedIndex >= 0 && _saves != null && listBoxSaves.SelectedIndex < _saves.Count)
+            {
+                var stateToDelete = _saves[listBoxSaves.SelectedIndex];
+                if (MessageBox.Show($"Вы уверены, что хотите удалить сохранение '{stateToDelete.SaveName}'?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    _saves.RemoveAt(listBoxSaves.SelectedIndex);
+                    _ownerFractalForm.SaveAllSavesForThisType(_saves);
+                    LoadSavesList(); // Перезагружаем список
+                }
+            }
+        }
+
+        private void UpdateButtonsState()
+        {
+            bool itemSelected = listBoxSaves.SelectedIndex != -1;
+            btnLoad.Enabled = itemSelected;
+            btnDelete.Enabled = itemSelected;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel; // Устанавливаем результат диалога
+            this.Close();
+        }
+
+        // Важно освобождать ресурсы при закрытии формы
+        private void SaveLoadDialogForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ClearPreview();
         }
     }
 }
