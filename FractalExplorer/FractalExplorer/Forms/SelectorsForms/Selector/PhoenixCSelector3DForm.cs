d7f3c439f4609cb4edbb3a1d15f70ffedd36e44f
xanstar6067 <district6067@gmail.com>
2025-06-26 11:15:49 +0300

    Добавил новое окно обновленного селектора. (Прототип. Без реализации.)

---
+++ FractalExplorer/FractalExplorer/Forms/SelectorsForms/Selector/PhoenixCSelector3DForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FractalExplorer.Forms.SelectorsForms.Selector
+{
+    public partial class PhoenixCSelector3DForm : Form
+    {
+        public PhoenixCSelector3DForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
