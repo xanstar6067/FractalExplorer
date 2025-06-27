8a759c16acbc6b0808e5e71e2ec5ef149a120d35
xanstar6067 <district6067@gmail.com>
2025-06-27 14:52:11 +0300

    Рефакторинг SaveStateImplementations

--- FractalExplorer/FractalExplorer/Utilities/SaveIO/SaveStateImplementations/FractalSaveStateBase.cs
+++ FractalExplorer/FractalExplorer/Utilities/SaveIO/SaveStateImplementations/FractalSaveStateBase.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FractalExplorer.Utilities.SaveIO.SaveStateImplementations
+﻿namespace FractalExplorer.Utilities.SaveIO.SaveStateImplementations
 {
+    /// <summary>
+    /// Представляет базовый класс для сохранения состояния любого типа фрактала.
+    /// Содержит общие метаданные и параметры для генерации предварительного просмотра.
+    /// </summary>
     public abstract class FractalSaveStateBase
     {
+        /// <summary>
+        /// Получает или задает имя сохранения, выбранное пользователем.
+        /// </summary>
         public string SaveName { get; set; }
+
+        /// <summary>
+        /// Получает или задает метку времени, когда было создано это состояние сохранения.
+        /// </summary>
         public DateTime Timestamp { get; set; }
-        public string FractalType { get; protected set; } // Устанавливается в конструкторе наследника
-        public string PreviewParametersJson { get; set; } // JSON с параметрами для превью
+
+        /// <summary>
+        /// Получает строковый идентификатор типа фрактала, к которому относится это сохранение.
+        /// Это свойство устанавливается в конструкторе производного класса.
+        /// </summary>
+        public string FractalType { get; protected set; }
+
+        /// <summary>
+        /// Получает или задает JSON-строку, содержащую параметры, необходимые для быстрого рендеринга
+        /// миниатюры или предварительного просмотра фрактала без загрузки полного состояния.
+        /// </summary>
+        public string PreviewParametersJson { get; set; }
     }
 }
