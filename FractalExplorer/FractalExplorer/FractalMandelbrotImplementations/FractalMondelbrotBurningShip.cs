2f756669a047dc8158506960720814f835613282
xanstar6067 <district6067@gmail.com>
2025-06-27 14:38:11 +0300

    Рефакторинг FractalMondelbrotBurningShip

--- FractalExplorer/FractalExplorer/FractalMandelbrotImplementations/FractalMondelbrotBurningShip.cs
+++ FractalExplorer/FractalExplorer/FractalMandelbrotImplementations/FractalMondelbrotBurningShip.cs
@@ -7,7 +7,8 @@ namespace FractalExplorer.Projects
 {
     /// <summary>
     /// Форма для отображения и взаимодействия с множеством "Пылающий корабль" Мандельброта.
-    /// Является конкретной реализацией базовой формы фрактала.
+    /// Является конкретной реализацией базовой формы фрактала,
+    /// предназначенной для отображения и управления параметрами фрактала "Пылающий корабль".
     /// </summary>
     public partial class FractalMondelbrotBurningShip : FractalMandelbrotFamilyForm
     {
@@ -38,17 +39,20 @@ namespace FractalExplorer.Projects
         /// <summary>
         /// Получает начальную координату X центра для фрактала "Пылающий корабль" Мандельброта.
         /// </summary>
+        // Это значение было выбрано как хороший центр для начального отображения фрактала "Пылающий корабль".
         protected override decimal InitialCenterX => -0.25m;
 
         /// <summary>
         /// Получает начальную координату Y центра для фрактала "Пылающий корабль" Мандельброта.
         /// </summary>
+        // Это значение было выбрано как хороший центр для начального отображения фрактала "Пылающий корабль".
         protected override decimal InitialCenterY => -0.5m;
 
         /// <summary>
         /// Вызывается после завершения инициализации формы.
-        /// Скрывает элементы управления, которые не требуются для данного фрактала
-        /// (например, поля для константы 'C' и панель предпросмотра).
+        /// Скрывает элементы управления, которые не требуются для данного фрактала,
+        /// такие как поля для константы 'C' и панель предпросмотра, поскольку
+        /// они специфичны для фракталов Жюлиа.
         /// </summary>
         protected override void OnPostInitialize()
         {
@@ -73,18 +77,35 @@ namespace FractalExplorer.Projects
 
         #region ISaveLoadCapableFractal Overrides
 
+        /// <summary>
+        /// Получает строковый идентификатор типа фрактала.
+        /// </summary>
         public override string FractalTypeIdentifier => "MandelbrotBurningShip";
 
+        /// <summary>
+        /// Получает конкретный тип состояния сохранения, используемый для множества "Пылающий корабль" Мандельброта.
+        /// </summary>
         public override Type ConcreteSaveStateType => typeof(MandelbrotFamilySaveState);
 
+        /// <summary>
+        /// Загружает все сохраненные состояния фрактала для данного типа.
+        /// </summary>
+        /// <returns>Список базовых объектов состояний фрактала.</returns>
         public override List<FractalSaveStateBase> LoadAllSavesForThisType()
         {
             var specificSaves = SaveFileManager.LoadSaves<MandelbrotFamilySaveState>(this.FractalTypeIdentifier);
+            // Преобразуем список конкретного типа в список базового типа для соответствия интерфейсу.
             return specificSaves.Cast<FractalSaveStateBase>().ToList();
         }
 
+        /// <summary>
+        /// Сохраняет предоставленный список состояний фрактала для данного типа.
+        /// </summary>
+        /// <param name="saves">Список базовых объектов состояний фрактала для сохранения.</param>
         public override void SaveAllSavesForThisType(List<FractalSaveStateBase> saves)
         {
+            // Преобразуем список базового типа обратно в список конкретного типа перед сохранением,
+            // так как SaveFileManager ожидает конкретный тип для сериализации.
             var specificSaves = saves.Cast<MandelbrotFamilySaveState>().ToList();
             SaveFileManager.SaveSaves(this.FractalTypeIdentifier, specificSaves);
         }
