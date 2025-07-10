a532b9eb20b6753616c98ad28591181f18cbf710
xanstar6067 <district6067@gmail.com>
2025-07-10 14:43:18 +0300

    Небольшие правки цветов.

--- FractalExplorer/FractalExplorer/Utilities/SaveIO/ColorPalettes/ColorPaletteMandelbrotFamily.cs
+++ FractalExplorer/FractalExplorer/Utilities/SaveIO/ColorPalettes/ColorPaletteMandelbrotFamily.cs
@@ -109,29 +109,17 @@ namespace FractalExplorer.Utilities.SaveIO.ColorPalettes
 
         // Новые оригинальные палитры
         new("Закат", new List<Color> { Color.Black, Color.FromArgb(255, 25, 25, 112), Color.FromArgb(255, 75, 0, 130), Color.FromArgb(255, 139, 0, 139), Color.FromArgb(255, 220, 20, 60), Color.FromArgb(255, 255, 140, 0), Color.FromArgb(255, 255, 215, 0), Color.White }, true, true, 600, 1.1, false),
-
         new("Океан", new List<Color> { Color.Black, Color.FromArgb(255, 0, 20, 40), Color.FromArgb(255, 0, 50, 80), Color.FromArgb(255, 0, 100, 150), Color.FromArgb(255, 0, 150, 200), Color.FromArgb(255, 100, 200, 255), Color.FromArgb(255, 200, 240, 255), Color.White }, true, true, 450, 1.0, false),
-
         new("Золото", new List<Color> { Color.Black, Color.FromArgb(255, 85, 65, 0), Color.FromArgb(255, 139, 115, 0), Color.FromArgb(255, 205, 173, 0), Color.FromArgb(255, 255, 215, 0), Color.FromArgb(255, 255, 235, 128), Color.FromArgb(255, 255, 248, 220), Color.White }, true, true, 300, 0.8, false),
-
         new("Медь", new List<Color> { Color.Black, Color.FromArgb(255, 72, 61, 20), Color.FromArgb(255, 138, 54, 15), Color.FromArgb(255, 184, 115, 51), Color.FromArgb(255, 205, 127, 50), Color.FromArgb(255, 240, 147, 43), Color.FromArgb(255, 255, 200, 124), Color.White }, true, true, 280, 0.9, false),
-
-        new("Неон", new List<Color> { Color.Black, Color.FromArgb(255, 75, 0, 75), Color.FromArgb(255, 255, 0, 255), Color.FromArgb(255, 0, 255, 255), Color.FromArgb(255, 0, 255, 0), Color.FromArgb(255, 255, 255, 0), Color.FromArgb(255, 255, 100, 255), Color.White }, true, true, 150, 1.3, false),
-
+        new("Неон", new List<Color> { Color.Black, Color.FromArgb(255, 75, 0, 75), Color.FromArgb(255, 255, 0, 255), Color.FromArgb(255, 0, 255, 255), Color.FromArgb(255, 0, 255, 0), Color.FromArgb(255, 255, 255, 0), Color.FromArgb(255, 255, 100, 255), Color.White }, true, true, 350, 1.3, false),
         new("Радуга", new List<Color> { Color.Black, Color.FromArgb(255, 148, 0, 211), Color.FromArgb(255, 75, 0, 130), Color.FromArgb(255, 0, 0, 255), Color.FromArgb(255, 0, 255, 0), Color.FromArgb(255, 255, 255, 0), Color.FromArgb(255, 255, 127, 0), Color.FromArgb(255, 255, 0, 0), Color.White }, true, true, 350, 1.0, false),
-
         new("Аметист", new List<Color> { Color.Black, Color.FromArgb(255, 25, 25, 112), Color.FromArgb(255, 72, 61, 139), Color.FromArgb(255, 123, 104, 238), Color.FromArgb(255, 147, 112, 219), Color.FromArgb(255, 221, 160, 221), Color.FromArgb(255, 238, 203, 238), Color.White }, true, true, 520, 1.1, false),
-
         new("Лес", new List<Color> { Color.Black, Color.FromArgb(255, 0, 39, 0), Color.FromArgb(255, 0, 69, 0), Color.FromArgb(255, 34, 139, 34), Color.FromArgb(255, 50, 205, 50), Color.FromArgb(255, 124, 252, 0), Color.FromArgb(255, 173, 255, 47), Color.White }, true, true, 380, 1.0, false),
-
         new("Космос", new List<Color> { Color.Black, Color.FromArgb(255, 25, 25, 112), Color.FromArgb(255, 72, 61, 139), Color.FromArgb(255, 138, 43, 226), Color.FromArgb(255, 255, 20, 147), Color.FromArgb(255, 255, 105, 180), Color.FromArgb(255, 255, 182, 193), Color.White }, true, true, 650, 1.2, false),
-
         new("Бирюза", new List<Color> { Color.Black, Color.FromArgb(255, 0, 100, 100), Color.FromArgb(255, 0, 139, 139), Color.FromArgb(255, 72, 209, 204), Color.FromArgb(255, 175, 238, 238), Color.FromArgb(255, 224, 255, 255), Color.White }, true, true, 420, 1.0, false),
-
         new("Лава", new List<Color> { Color.Black, Color.FromArgb(255, 139, 0, 0), Color.FromArgb(255, 205, 0, 0), Color.FromArgb(255, 255, 69, 0), Color.FromArgb(255, 255, 140, 0), Color.FromArgb(255, 255, 215, 0), Color.FromArgb(255, 255, 255, 224), Color.White }, true, true, 250, 0.7, false),
-
         new("Монохром синий", new List<Color> { Color.Black, Color.FromArgb(255, 0, 0, 139), Color.FromArgb(255, 0, 0, 205), Color.FromArgb(255, 65, 105, 225), Color.FromArgb(255, 135, 206, 235), Color.FromArgb(255, 176, 224, 230), Color.White }, true, true, 480, 1.0, false),
-
         new("Монохром красный", new List<Color> { Color.Black, Color.FromArgb(255, 139, 0, 0), Color.FromArgb(255, 205, 0, 0), Color.FromArgb(255, 220, 20, 60), Color.FromArgb(255, 255, 105, 180), Color.FromArgb(255, 255, 182, 193), Color.White }, true, true, 320, 1.0, false)
     };
         }
