7bb0f1fbbd4943cc2cd8699aef8ae035a7667b3f
xanstar6067 <district6067@gmail.com>
2025-07-14 16:56:14 +0300

    Добавление функции самотета. (только форма)

---
+++ FractalExplorer/FractalExplorer/Forms/Other/TestingFractalSystemForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FractalExplorer.Forms.Other
+{
+    public partial class TestingFractalSystemForm : Form
+    {
+        public TestingFractalSystemForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
