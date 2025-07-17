a07154ec3ef663cde843689e8e702036f93f65f2
xanstar6067 <district6067@gmail.com>
2025-07-17 16:48:05 +0300

    FractalCollatzForm

---
+++ FractalExplorer/FractalExplorer/Forms/Fractals/FractalCollatzForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FractalExplorer.Forms.Fractals
+{
+    public partial class FractalCollatzForm : Form
+    {
+        public FractalCollatzForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
