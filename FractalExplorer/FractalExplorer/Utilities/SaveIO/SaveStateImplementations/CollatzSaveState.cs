2d4ad213d5ba498fae2253ddc528f26514567a20
xanstar6067 <district6067@gmail.com>
2025-07-18 13:08:15 +0300

    Начало интеграции функции сохранения состояний.

---
+++ FractalExplorer/FractalExplorer/Utilities/SaveIO/SaveStateImplementations/CollatzSaveState.cs
@@ -0,0 +1,26 @@
+﻿namespace FractalExplorer.Utilities.SaveIO.SaveStateImplementations
+{
+    /// <summary>
+    /// Представляет состояние сохранения для фрактала, основанного на гипотезе Коллатца.
+    /// Наследует все основные параметры от MandelbrotFamilySaveState.
+    /// </summary>
+    public class CollatzSaveState : MandelbrotFamilySaveState
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="CollatzSaveState"/>.
+        /// Этот конструктор предназначен для использования десериализатором JSON.
+        /// </summary>
+        public CollatzSaveState() : base()
+        {
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="CollatzSaveState"/>
+        /// с указанным идентификатором типа фрактала.
+        /// </summary>
+        /// <param name="fractalType">Строковый идентификатор типа фрактала (например, "Collatz").</param>
+        public CollatzSaveState(string fractalType) : base(fractalType)
+        {
+        }
+    }
+}
