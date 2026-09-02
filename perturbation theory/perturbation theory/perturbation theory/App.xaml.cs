de7749d7abbfa75bb50bde20b9d73af5c2b4f04d
xanstar6067 <district6067@gmail.com>
2026-09-02 13:36:04 +0300

    Создан тестовый проект perturbation theory

---
+++ perturbation theory/perturbation theory/perturbation theory/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace perturbation_theory
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
